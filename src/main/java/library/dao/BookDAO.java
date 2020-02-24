864db7b870f17953a0d6376ece3e8e07e17acd22
kasper084 <kasper084@gmail.com>
2020-02-24 13:25:32 +0200

    80% done

--- src/main/java/library/dao/BookDAO.java
+++ src/main/java/library/dao/BookDAO.java
@@ -1,9 +1,7 @@
 package library.dao;
 
 import library.entity.Book;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
